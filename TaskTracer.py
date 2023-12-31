eece6670e035b95c90b8bc6a11adaa1f74dcaa38
06_mini <06_mini@proton.me>
2024-01-01 07:35:10 +0900

    v4_SQLite_GPT

--- TaskTracer.py
+++ TaskTracer.py
@@ -1,72 +1,67 @@
 import sqlite3
 import datetime
 
-def selection():
-    while True:
-        choice = input("\nSelect number |Record today [1]| Result today [2]|: ")
-        if choice == '1':
-            execute_main()
-            break
+# understood
+def create_table(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS productivity (
+                        date TEXT,
+                        time INTEGER,
+                        rate INTEGER,
+                        tasks TEXT)''')
 
-        elif choice == '2':
-            computing()
-            break
+# understood
+def insert_record(cursor, date, hour, rate, tasks):
+    cursor.execute('''INSERT INTO productivity (date, time, rate, tasks) VALUES (?, ?, ?, ?)''', (date, hour, rate, tasks))
 
-        else:
-            print("Invalid input, please try again.")
+# understood
+def get_average_productivity(cursor, date):
+    cursor.execute('''SELECT AVG(rate) FROM productivity WHERE date = ?''', (date,))
+    result = cursor.fetchone()
+    return result[0] if result[0] is not None else None
 
-def execute_main():
-    
-
-
+# understood
+def record_productivity(cursor, date):
     for hour in range(7, 22):
         time_label = f"{hour % 12 if hour != 12 else 12} {'PM' if hour >= 12 else 'AM'}"
         tasks = input(f"\nWhat did you do until {time_label}?: ").strip()
-        c.execute(f'''CREATE TABLE IF NOT EXISTS {now2} (time INTEGER,rate INTEGER,tasks TEXT) ''')
-        
 
         while True:
             try:
                 rate = int(input("\nRate your productivity [1-10]: "))
                 if 1 <= rate <= 10:
-                    c.execute(f'''INSERT INTO {now2} VALUES ({hour},{rate},"{tasks}")''')
-                    conn.commit()
+                    insert_record(cursor, date, hour, rate, tasks)
                     break
-
-                else:           
+                else:
                     print("Please enter a number between 1 and 10.")
-
             except ValueError:
                 print("Invalid input. Please enter a number.")
 
+# understood
+def main():
+    conn = sqlite3.connect('MyHistory.db')
+    cursor = conn.cursor()
+    create_table(cursor)
 
-def computing():
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
 
-    try:
-        average = 0
-        j = 0
-        for row in c.execute(f'''SELECT rate FROM {now2}'''):
-            j+=1
-            for i in row:
-                average +=i           
-
-        if row:
-            average /= j 
-            print(f"Average productivity rate for today is {average:.2f}")
-
+    while True:
+        choice = input("\nSelect number |Record today [1]| Result today [2]| Exit [3]: ")
+        if choice == '1':
+            record_productivity(cursor, today)
+        elif choice == '2':
+            average = get_average_productivity(cursor, today)
+            if average is not None:
+                print(f"Average productivity rate for today is {average:.2f}")
+            else:
+                print("No productivity data available for today.")
+        elif choice == '3':
+            break
         else:
-            print("No productivity data available for today.")
+            print("Invalid input, please try again.")
 
-    except KeyError:
-        print("No data available for today.")
+    conn.commit()
+    conn.close()
 
-
-now = {datetime.datetime.now().strftime("%Y-%m-%d")}
-now2 = f"\"{now}\""
-
-conn = sqlite3.connect('MyHistory.db')
-c = conn.cursor()
-
-selection()
-
-conn.close()
+# understood
+if __name__ == "__main__":
+    main()
